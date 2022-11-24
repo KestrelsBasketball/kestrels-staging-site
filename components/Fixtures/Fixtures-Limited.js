88fe1f0664e51a257aa2c12722803712dbf301c6
shanewmurphy <shn.murphy@gmail.com>
2022-11-24 20:09:48 +0000

    Fixture Card Mobile

--- components/Fixtures/Fixtures-Limited.js
+++ components/Fixtures/Fixtures-Limited.js
@@ -33,43 +33,61 @@ const fixturesLimited = [
 
 export default function FixturesLimited() {
   return (
-    <div className={styles.container}>
-      <Grid.Container>
-        <Grid xs={12} sm={4} md={4}>
-          AAAAA
-        </Grid>
-        <Grid
-          xs={12}
-          sm={8}
-          md={8}
-          justify="center"
-          className={styles.fixtures_container}
-        >
-          <div className={styles.fixture_container}>
-            <div className={styles.teams_container}>
-              <div>
-                <h6>MABB Men’s League</h6>
+    <div>
+      <div className={styles.container}>
+        <Grid.Container>
+          <Grid xs={12} sm={4} md={4}>
+            AAAAA
+          </Grid>
+          <Grid
+            xs={12}
+            sm={8}
+            md={8}
+            justify="center"
+            className={styles.fixtures_container}
+          >
+            <div className={styles.fixture_container}>
+              <div className={styles.teams_container}>
+                <div>
+                  <h6>MABB Men’s League</h6>
+                </div>
+                <div className={styles.teams}>
+                  <div>
+                    <h5>Kestrels</h5>
+                  </div>
+                  <div>
+                    <span>VS</span>
+                  </div>
+                  <div>
+                    <h5 className={styles.h5_last}>Kildare Gliders BC</h5>
+                  </div>
+                </div>
               </div>
-              <div className={styles.teams}>
-                <div>
-                  <h5>Kestrels</h5>
-                </div>
-                <div>
-                  <span>VS</span>
-                </div>
-                <div>
-                  <h5 className={styles.h5_last}>Kildare Gliders BC</h5>
-                </div>
+              <div className={styles.game_details}>
+                <div>Colaiste losagain, Portarlington</div>
+                <div>Friday 18 November 2022</div>
+                <div>19:30</div>
               </div>
             </div>
-            <div className={styles.game_details}>
-              <div>Colaiste losagain, Portarlington</div>
-              <div>Friday 18 November 2022</div>
-              <div>19:30</div>
-            </div>
+          </Grid>
+        </Grid.Container>
+      </div>
+
+      {/*  Mobile Fixture layout  */}
+      <div className={styles.fixtures_layout_mobile}>
+        <div className={styles.fixture_card_mobile}>
+          <div className={styles.mobile_card_styles}>
+            <h6>MABB Men’s League</h6>
+            <h3>Kestrels</h3>
+            <span>VS</span>
+            <h3>Kildare Gliders BC</h3>
+            <div className={styles.divider}></div>
+            <h4>Colaiste losagain, Portarlington</h4>
+            <h5>Friday 18 November 2022</h5>
+            <p>19:30</p>
           </div>
-        </Grid>
-      </Grid.Container>
+        </div>
+      </div>
     </div>
   );
 }
