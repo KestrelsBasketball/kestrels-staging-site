f059549031df54148e555b80402f3cc9f8a60993
shanewmurphy <shn.murphy@gmail.com>
2022-11-25 15:23:22 +0000

    Re-do Fixtures desktop - mobile carousel swiper for fixtures

--- components/Fixtures/Fixtures-Limited.js
+++ components/Fixtures/Fixtures-Limited.js
@@ -1,5 +1,10 @@
 import styles from "./fixtures.module.css";
-import { Grid, Image, Row, Col } from "@nextui-org/react";
+import { Grid } from "@nextui-org/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const fixturesLimited = [
   {
@@ -15,16 +20,16 @@ const fixturesLimited = [
     id: "02",
     tournament: "Juvenile U14 Boys Div 2",
     TeamA: "kestrels",
-    TeamB: "Kildare Gliders BC",
-    Location: "Colaiste losagain, Portarlington",
+    TeamB: "Longford Falcons BC",
+    Location: "Old Sports Hall, Maynooth PP",
     Date: "Friday 22 November 2022",
-    Time: "20:30",
+    Time: "17:30",
   },
   {
     id: "03",
     tournament: "Juvenile U14 Boys Div 2",
     TeamA: "kestrels",
-    TeamB: "Kildare Gliders BC",
+    TeamB: "Portlaoise Panthers BC",
     Location: "Colaiste losagain, Portarlington",
     Date: "Friday 22 November 2022",
     Time: "20:30",
@@ -37,57 +42,79 @@ export default function FixturesLimited() {
       <div className={styles.container}>
         <Grid.Container>
           <Grid xs={12} sm={4} md={4}>
-            AAAAA
+            <div className={styles.fixture_graphic_container}>ss</div>
           </Grid>
-          <Grid
-            xs={12}
-            sm={8}
-            md={8}
-            justify="center"
-            className={styles.fixtures_container}
-          >
-            <div className={styles.fixture_container}>
-              <div className={styles.teams_container}>
-                <div>
-                  <h6>MABB Men’s League</h6>
-                </div>
-                <div className={styles.teams}>
-                  <div>
-                    <h5>Kestrels</h5>
-                  </div>
-                  <div>
-                    <span>VS</span>
-                  </div>
-                  <div>
-                    <h5 className={styles.h5_last}>Kildare Gliders BC</h5>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.game_details}>
-                <div>Colaiste losagain, Portarlington</div>
-                <div>Friday 18 November 2022</div>
-                <div>19:30</div>
-              </div>
+          <Grid xs={12} sm={8} md={8} justify="center">
+            <div className={styles.fixtures_deskop_layout}>
+              <ul>
+                {fixturesLimited.map((fixture) => {
+                  return (
+                    <li key={fixture.id}>
+                      <div className={styles.fixture_Card}>
+                        <div className={styles.fixture_tournament_title}>
+                          <h6>{fixture.tournament}</h6>
+                        </div>
+                        <div className={styles.teams}>
+                          <div>
+                            <h5>{fixture.TeamA}</h5>
+                          </div>
+                          <div>
+                            <span>VS</span>
+                          </div>
+                          <div>
+                            <h5 className={styles.h5_left_align}>
+                              {fixture.TeamB}
+                            </h5>
+                          </div>
+                        </div>
+                        <div className={styles.game_details_container}>
+                          <div>{fixture.Location}</div>
+                          <div>{fixture.Date}</div>
+                          <div>{fixture.Time}</div>
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </Grid>
         </Grid.Container>
-      </div>
-
-      {/*  Mobile Fixture layout  */}
-      <div className={styles.fixtures_layout_mobile}>
-        <div className={styles.fixture_card_mobile}>
-          <div className={styles.mobile_card_styles}>
-            <h6>MABB Men’s League</h6>
-            <h3>Kestrels</h3>
-            <span>VS</span>
-            <h3>Kildare Gliders BC</h3>
-            <div className={styles.divider}></div>
-            <div className={styles.game_details_mobile}>
-              <h4>Colaiste losagain, Portarlington</h4>
-              <h4>Friday 18 November 2022</h4>
-              <p>19:30</p>
-            </div>
-          </div>
+        <div className={styles.fixtures_carousel_container}>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+          >
+            {fixturesLimited.map((fixture) => {
+              return (
+                <SwiperSlide>
+                  <div className={styles.carousel_card}>
+                    <div className={styles.carousel_teams}>
+                      <div>
+                        <h6>{fixture.tournament}</h6>
+                      </div>
+                      <div>
+                        <h5>{fixture.TeamA}</h5>
+                      </div>
+                      <div>
+                        <span>VS</span>
+                      </div>
+                      <div>
+                        <h5>{fixture.TeamB}</h5>
+                      </div>
+                    </div>
+                    <hr className={styles.solid}></hr>
+                    <div className={styles.game_details}>
+                      <div>AA</div>
+                      <div>BB</div>
+                      <div>CC</div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
         </div>
       </div>
     </div>
